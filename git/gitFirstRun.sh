48f45b17c422041017953aea0c4c6cf9c1172a19
Jelmergu <jelmergujelmer@hotmail.com>
2016-11-15 21:28:22 +0100

    Added choice to set the aliases to gitFirstRun

--- git/gitFirstRun.sh
+++ git/gitFirstRun.sh
@@ -21,11 +21,23 @@ function gitFirstRun {
     git config --global init.templatedir '~/.git_template'
     git config --global core.excludesfile '~/.gitignore'
 
-    # Adding some aliasses
-    git config --global alias.a 'add .'
-    git config --global alias.st 'status'
-    git config --global alias.ct 'commit -F gitCommit.txt'
-    git config --global alias.ci 'commit'
+    read -p "Add usefull aliases? (y/n): " choice
 
+    while [ "${choice}" != "y" -a "${choice}" != "n" ]
+    do
+        read -p "${choice} is invalid, please use either 'y' or 'n': " choice
+    done
+
+    if [ "${choice}" == "y" ]
+        then
+        # Adding some usefull aliasses
+        git config --global alias.a 'add .'
+        git config --global alias.st 'status'
+        git config --global alias.ct '!git commit -F $(git rev-parse --git-dir)/../gitCommit.txt'
+        git config --global alias.ci 'commit'
+        git config --global alias.ca '!git a && git ci'
+        git config --global alias.co 'checkout'
+        git config --global alias.cm 'checkout master'
+    fi
     echo "Done"
 }
