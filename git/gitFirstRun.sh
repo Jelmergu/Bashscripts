1ff3e4d153eef9941b43243fb49ecbc01c61c32c
Jelmergu <jwijnja@a-vision.nu>
2017-06-19 15:59:21 +0200

    Update gitFirstRun to include more aliases

--- git/gitFirstRun.sh
+++ git/gitFirstRun.sh
@@ -67,32 +67,48 @@ function gitFirstRun {
     if [ "${choice}" == "y" ]
         then
         # Adding some usefull aliasses
-
-        git config --global alias.st 'status -s'
         git config --global alias.a 'add -A'
-        git config --global alias.ct '!git commit -s -F $(git rev-parse --git-dir)/../gitCommit.txt'
+        git config --global alias.st 'status -s'
+        git config --global alias.ct '!git commit -F $(git rev-parse --git-dir)/../gitCommit.txt'
+
         git config --global alias.ci 'commit -s'
-        git config --global alias.am '!git a && git commit --amend'
         git config --global alias.ca '!git a && git ci'
         git config --global alias.cat '!git a && git ct'
+        git config --global alias.amend '!git a && git ci --amend'
+
         git config --global alias.co 'checkout'
         git config --global alias.cm 'checkout master'
-        # Git flow aliases
-        git config --global alias.f 'flow'
-        git config --global alias.ff 'flow feature'
-        git config --global alias.ffs 'flow feature start'
-        git config --global alias.fff 'flow feature finish'
-        git config --global alias.ffp 'flow feature publish'
-        git config --global alias.fr 'flow release'
-        git config --global alias.frs 'flow release start'
-        git config --global alias.frf 'flow release finish'
-        git config --global alias.frp 'flow release publish'
-        git config --global alias.fh 'flow hotfix'
-        git config --global alias.fhs 'flow hotfix start'
-        git config --global alias.fhf 'flow hotfix finish'
-        git config --global alias.fhp 'flow hotfix publish'
+        git config --global alias.cd 'checkout develop'
 
+        read -p "Add git flow aliases? (y/n): " choice
+
+        while [ "${choice}" != "y" -a "${choice}" != "n" ]
+        do
+            read -p "${choice} is invalid, please use either 'y' or 'n': " choice
+        done
+
+        if [ "${choice}" == "y" ]
+            then
+             # Git flow aliases
+            git config --global alias.f 'flow'
+
+            git config --global alias.ff 'flow feature'
+            git config --global alias.ffs 'flow feature start'
+            git config --global alias.fff 'flow feature finish'
+            git config --global alias.ffp 'flow feature publish'
+
+            git config --global alias.fr 'flow release'
+            git config --global alias.frs 'flow release start'
+            git config --global alias.frf 'flow release finish'
+            git config --global alias.frp 'flow release publish'
+
+            git config --global alias.fh 'flow hotfix'
+            git config --global alias.fhs 'flow hotfix start'
+            git config --global alias.fhf 'flow hotfix finish'
+            git config --global alias.fhp 'flow hotfix publish'
+        fi
 
     fi
+
     echo "Done"
 }
