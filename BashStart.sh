9b6f4c71ed08899fd1071cff4aa43f8d1b4a4617
Jelmergu <jelmergujelmer@hotmail.com>
2018-05-10 17:20:11 +0200

    Add selfupdater
    Signed-off-by: Jelmergu <jelmergujelmer@hotmail.com>

--- BashStart.sh
+++ BashStart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+bashLibraryDir=$(dirname "${BASH_SOURCE[0]}")
+
 function includeDirectory {
     local dir=${1}
 
@@ -10,9 +12,10 @@ function includeDirectory {
 
     for d in "${dir}"/*
     do
+
         if [ "${d}" == "." -o "${d}" == ".." -o "${d}" == "${BASH_SOURCE[0]}" ]
          then
-            echo "" > /dev/null
+            echo > /dev/null
         elif [ -d "${d}" ]
         then
             includeDirectory ${d}
@@ -23,4 +26,19 @@ function includeDirectory {
     done
 }
 
-includeDirectory $(dirname "${BASH_SOURCE[0]}")
+function selfUpdate {
+    local branchStatus=$(cd "${bashLibraryDir}" && git fetch origin && git status -b --porcelain)
+
+    if [[ "$branchStatus" == *"behind"* ]]
+    then
+        if [[ $BASHLIBRARY_AUTOUPDATE = true ]]
+        then
+            (cd dirname "${BASH_SOURCE[0]}" && git pull --ff-only)
+        else
+            echo -e "BashLibrary is outdated and not allowed to autoupdate\n Run (cd $(dirname ${BASH_SOURCE[0]}) && git pull --ff-only) to update"
+            echo -e "\t Alternativly you could set BASHLIBRARY_AUTOUPDATE to true to perform automatic updates"
+        fi
+    fi
+}
+
+selfUpdate && includeDirectory "${bashLibraryDir}"
